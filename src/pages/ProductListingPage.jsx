7d8c0aca7454140b06f689d1874c38b5ab0f5df5
Rafael Santos <raffasatos10@gmail.com>
2024-10-02 14:21:36 -0300

    pagina de filtro responsiva

--- src/pages/ProductListingPage.jsx
+++ src/pages/ProductListingPage.jsx
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../pages/Layout";
 import FilterGroup from "../components/FilterGroup";
 import ProductListing from "../components/ProductListing";
+import FrameFilter from "../assets/FrameFilter.png"
 
 // import { Container } from './styles';
 
 const ProductListingPage = () => {
+
+const [ filterModalOpen, setFilterModalOpen] = useState(false)
+
+function openModal(){
+  setFilterModalOpen(!filterModalOpen)
+  console.log(filterModalOpen)
+}
+
+
   const filterOptions = [
     {
       title: "Marca",
@@ -37,7 +47,30 @@ const ProductListingPage = () => {
   return (
     <>
       <Layout>
-        <div className="flex mt-6">
+        <div className="flex gap-4  my-6 lg:my-0 flex-col lg:flex-row lg:justify-between items-center  w-full   lg:p-20">
+          <div className="flex items-center gap-4">
+            <div>
+              <label className="border-black border p-4 ">
+                Ordenar por:
+                <select name="orderProducts">
+                  <option value="relevancia">mais relevantes</option>
+                  <option value="menorPreco">menor preço</option>
+                  <option value="maiorPreco">maior preço</option>
+                </select>
+              </label>
+            </div>
+            <div className="lg:hidden w-16 h-16" onClick={openModal}>
+              <img src={FrameFilter} alt="" />
+            </div>
+          </div>
+
+          <p className="text-base font-semibold  ">
+            Resultados para "<span>Tênis</span>" -{" "}
+            <span className="font-normal leading-7 ">309 produtos</span>
+          </p>
+        </div>
+
+        <section className="flex mt-2 lg:mt-6">
           <div className="bg-white hidden lg:block px-4 pt-3 w-[400px] h-fit mx-8">
             <h4 className="text-dark-gray-2 font-semibold">Filtrar por</h4>
             <hr className="border-light-gray-2 my-3" />
@@ -63,7 +96,7 @@ const ProductListingPage = () => {
             />
           </div>
           <ProductListing />
-        </div>
+        </section>
       </Layout>
     </>
   );
