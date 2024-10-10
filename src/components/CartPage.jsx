b58914ed23760b05bc4458b4c5f99d0087bc3310
Breno Oliveira <135337339+breno-oliveira98@users.noreply.github.com>
2024-10-10 20:55:06 +0000

    primeira etapa do componente

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -21,28 +21,35 @@ export const CartPage = () => {
                     <div>
                         <h3 className="font-bold">MEU CARRINHO</h3>
                     </div>
-                    <div className="flex space-x-14">
+                    <div className="flex space-x-16">
                         <h3>QUANTIDADE</h3>
                         <h3>UNITÁRIO</h3>
                         <h3>TOTAL</h3>
                     </div>
                 </div>
                 {/* body */}
-                <div className="flex">
-                    <div className="bg-indigo-300 border w-[120px] h-[100px] flex justify-center rounded">
+                <div className="flex items-center border-b justify-between py-2">
+                    <div className="bg-indigo-300 border w-[120px] h-[90px] my-2 flex justify-center rounded">
                     <img src={tenis} className="w-auto h-auto  " alt="" />
                     </div>
 
-                    <div className="flex">
-                        <p></p>
-                        <p></p>
-                        <p></p>
+                    <div>
+                        <p className="font-bold">Titulo Produto</p>
+                        <p>Cor do Produto</p>
+                        <p>Tamanho do Produto</p>
                     </div>
 
                     <div className="flex">
                         <button id="box" className="flex bg-white border w-5 h-5 justify-center p-5 items-center rounded" onClick={removeItemBox}>-</button>
-                        <div id="valueBox" className="flex">{valorBox}</div>
+                        <div id="valueBox" className="flex mx-3 items-center">{valorBox}</div>
                         <button id="box" className="flex bg-white border w-5 h-5 justify-center p-5 items-center rounded" onClick={addItemBox}>+</button>
+                    </div>
+
+                    <div>
+                        <p id="preco" className="fs-1 font-bold">R$ 219,00</p>
+                    </div>
+                    <div>
+                        <p id="preco" className="fs-1 font-bold">R$ 219,00</p>
                     </div>
 
                 </div>
