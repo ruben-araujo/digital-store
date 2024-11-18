52492d6018b555e5cbc3bd5bae189173cb0a5aba
breno-oliveira98 <breno.oliveira2011@hotmail.com>
2024-11-18 18:06:15 -0300

    meus pedidos funcionando

--- src/pages/MyOrders.jsx
+++ src/pages/MyOrders.jsx
@@ -1,10 +1,11 @@
 import { MenuProfile } from "../components/MenuProfile";
+import ProductItens from "../database/ProductItens";
 
 export const MyOrders = () => {
   return (
     <div className="flex h-[84vh] justify-around py-4">
       <MenuProfile/>
-      <section className="w-[890px] h-fit bg-[var(--white)] border border-solid border-gray-500">
+      <section className="w-[890px] h-auto bg-[var(--white)] rounded-md p-5 overflow-scroll">
         <table className="w-full">
           <thead className="border-b">
             <tr>
@@ -13,10 +14,15 @@ export const MyOrders = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td className="text-left p-4">Conteúdo da primeira coluna</td>
-              <td className="text-right p-4">Conteúdo da segunda coluna</td>
+            {ProductItens.map((p, i) => (
+              <tr key={i} className="border-b">
+              <td className="text-left p-4 flex items-center gap-2">
+                <img src={p.image[0]} alt="" style={{maxWidth: "55px" }} />
+                <p className="color-[var(--dark-gray)]"><strong>{p.name}</strong></p>
+              </td>
+              <td className="text-right p-4">Pedido em trânsito</td>
             </tr>
+            ))}
           </tbody>
         </table>
       </section>
